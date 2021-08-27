e4d918d240c547d83ba4c0b81a0df2f0d5bb4eef
xxnsummer@gmail.com <xxnsummer@gmail.com>
2021-08-26 22:21:31 -0400

    last commit for today

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Plant, Gardening
+from .models import Plant, Gardening, Photo
 
 # Register your models here.
 admin.site.register(Plant)
 admin.site.register(Gardening)
+admin.site.register(Photo)
