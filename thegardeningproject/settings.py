7815298fe5aba98ac2e48046610559e4beb2810f
xxnsummer@gmail.com <xxnsummer@gmail.com>
2021-08-27 12:19:15 -0400

    Config deployment

--- thegardeningproject/settings.py
+++ thegardeningproject/settings.py
@@ -18,7 +18,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -135,3 +134,7 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
